414995e7bbd92d9cd2832619d4cd3b0420ec0baa
Hussain-2002 <Hussainmesharwala@gmail.com>
2026-09-12 11:24:02 +0530

    hussain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 
 import { AppProviders } from "@/components/site/AppProviders";
+import { GoogleAnalytics } from "@/components/site/GoogleAnalytics";
 import { SiteFonts } from "@/components/site/SiteFonts";
 import { getCatalog } from "@/lib/catalog.server";
 import { JsonLd, buildRootMetadata, organizationJsonLd, websiteJsonLd } from "@/lib/seo";
@@ -18,6 +19,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
         <link rel="alternate" type="text/plain" href="/llms.txt" title="LLMs.txt" />
       </head>
       <SiteFonts>
+        <GoogleAnalytics />
         <JsonLd data={[organizationJsonLd(), websiteJsonLd()]} />
         <AppProviders catalog={catalog}>{children}</AppProviders>
       </SiteFonts>
